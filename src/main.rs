999046ce120218228de913ebd66a70367d1bc04a
David Pedersen <david.pdrsn@gmail.com>
2019-02-15 17:44:56 +0100

    hacks

---
+++ src/main.rs
@@ -0,0 +1,174 @@
+use colored::*;
+use git2::BranchType;
+use git2::Repository;
+use lazy_static::lazy_static;
+use rayon::prelude::*;
+use regex::Regex;
+use reqwest;
+use serde_derive::Deserialize;
+use serde_json::Value;
+use std::collections::HashSet;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    /// Circleci token
+    ///
+    /// This argument is optional, if not provided it will look for a CIRCLECI_TOKEN environment
+    /// variable
+    #[structopt(long = "token", short = "t")]
+    token: Option<String>,
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+    let token = if let Some(token) = args.token {
+        token
+    } else {
+        use std::env;
+        env::var("CIRCLECI_TOKEN")
+            .expect("Missing --token argument, or CIRCLECI_TOKEN environment variable")
+    };
+
+    let url = format!(
+        "https://circleci.com/api/v1.1/recent-builds?circle-token={token}&limit={limit}",
+        token = token,
+        limit = 50,
+    );
+
+    let builds = reqwest::get(&url).unwrap().json::<Vec<Build>>().unwrap();
+    let builds = find_builds(builds);
+    print_builds(builds);
+}
+
+fn find_builds(mut builds: Vec<Build>) -> Vec<Build> {
+    builds.sort_unstable_by_key(|build| -build.build_num);
+
+    let repo = Repository::init(".").expect("No .git folder found");
+
+    let mut builds = builds
+        .into_iter()
+        .fold(Vec::<Build>::new(), |mut acc, build| {
+            if acc.iter().find(|b| b.branch == build.branch).is_none() {
+                acc.push(build);
+            }
+            acc
+        })
+        .into_iter()
+        .filter(|build| repo.find_branch(&build.branch, BranchType::Local).is_ok())
+        .collect::<Vec<_>>();
+
+    builds.sort_unstable_by_key(|build| {
+        if &build.branch == "master" {
+            "aaaa".to_string()
+        } else if &build.branch == "staging" {
+            "aaaaaaa".to_string()
+        } else if &build.branch == "develop" {
+            "aaaaaaaaaa".to_string()
+        } else {
+            build.branch.clone()
+        }
+    });
+
+    builds
+}
+
+fn print_builds(builds: Vec<Build>) {
+    let length = builds
+        .iter()
+        .max_by_key(|build| build.branch.len())
+        .unwrap()
+        .branch
+        .len();
+
+    for build in builds {
+        let branch = pad(&build.branch, length - build.branch.len());
+        let build_num = if build.outcome.failed() {
+            format!("{}", build.build_num)
+        } else {
+            "".to_string()
+        };
+
+        println!(
+            "{branch} {outcome} {build_num}",
+            branch = branch,
+            outcome = build.outcome.term_string(),
+            build_num = build_num,
+        );
+    }
+}
+
+fn pad(s: &str, n: usize) -> String {
+    let mut acc = s.to_string();
+    for _ in 0..n {
+        acc = format!(" {}", acc);
+    }
+    acc
+}
+
+#[derive(Debug, Deserialize)]
+struct Build {
+    branch: String,
+    build_num: i32,
+    outcome: Outcome,
+}
+
+#[derive(Debug, Deserialize)]
+enum Outcome {
+    #[serde(rename = "retried")]
+    Retried,
+    #[serde(rename = "canceled")]
+    Canceled,
+    #[serde(rename = "infrastructure_fail")]
+    InfrastructureFail,
+    #[serde(rename = "timedout")]
+    Timedout,
+    #[serde(rename = "not_run")]
+    NotRun,
+    #[serde(rename = "running")]
+    Running,
+    #[serde(rename = "failed")]
+    Failed,
+    #[serde(rename = "queued")]
+    Queued,
+    #[serde(rename = "scheduled")]
+    Scheduled,
+    #[serde(rename = "not_running")]
+    NotRunning,
+    #[serde(rename = "no_tests")]
+    NoTests,
+    #[serde(rename = "fixed")]
+    Fixed,
+    #[serde(rename = "success")]
+    Success,
+}
+
+impl Outcome {
+    fn term_string(&self) -> String {
+        use Outcome::*;
+
+        match self {
+            Retried => "retried".to_string(),
+            Canceled => "canceled".to_string(),
+            InfrastructureFail => "infrastructure fail".red().to_string(),
+            Timedout => "timeout".red().to_string(),
+            NotRun => "not run".to_string(),
+            Running => "running".blue().to_string(),
+            Failed => "failed".red().to_string(),
+            Queued => "queued".to_string(),
+            Scheduled => "scheduled".magenta().to_string(),
+            NotRunning => "not running".to_string(),
+            NoTests => "no tests".to_string(),
+            Fixed => "ok".green().to_string(),
+            Success => "ok".green().to_string(),
+        }
+    }
+
+    fn failed(&self) -> bool {
+        match self {
+            Outcome::Failed => true,
+            _ => false,
+        }
+    }
+}
